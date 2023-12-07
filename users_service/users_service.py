f6e092b020417e0192a738afd52d1bbd2b93baff
juliantello <yilver.tello@correounivalle.edu.co>
2023-12-07 16:31:05 -0500

    login correjido

--- users_service/users_service.py
+++ users_service/users_service.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
 import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_cors import CORS  # Importa la extensión
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Configura CORS para la aplicación
+CORS(app)
 app.config['SECRET_KEY'] = 'mi_clave_secreta_jwt'
 
 # Simulación de una base de datos (deberías usar una base de datos real)
@@ -13,12 +13,21 @@ users_db = []
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
+    if not data:
+        return jsonify({'message': 'Datos no proporcionados'}), 400
+
     username = data.get('username')
     email = data.get('email')
     nombre = data.get('nombre')
     apellido = data.get('apellido')
     password = data.get('password')
     telefono = data.get('telefono')
+
+    if not username or not email or not nombre or not apellido or not password or not telefono:
+        return jsonify({'message': 'Todos los campos son obligatorios'}), 400
+
+    if any(user['username'] == username for user in users_db):
+        return jsonify({'message': 'Nombre de usuario ya existe'}), 400
 
     hashed_password = generate_password_hash(password, method='sha256')
     user = {
@@ -37,23 +46,26 @@ def register():
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
+    if not data:
+        return jsonify({'message': 'Datos no proporcionados'}), 400
+
     username = data.get('username')
     password = data.get('password')
-    
+
     user = next((u for u in users_db if u['username'] == username), None)
+
     try:
-        if user and check_password_hash(user['password'], password):
-            token = jwt.encode({'username': username}, app.config['SECRET_KEY'])
-            return jsonify({'token': token.decode('UTF-8')}), 200
+        if user is not None and check_password_hash(user['password'], password):
+            token = jwt.encode({'username': username}, app.config['SECRET_KEY'], algorithm='HS256')
+            return jsonify({'token': token}), 200
+            
         else:
             print(f"Error en el inicio de sesión para el usuario {username}")
             return jsonify({'message': 'Credenciales inválidas'}), 401
+    
     except Exception as e:
         print(f"Error durante el inicio de sesión: {str(e)}")
-        return jsonify({'message': 'Error interno durante el inicio de sesión'}), 500
-
-
-
+        return jsonify({'message': f'Error interno durante el inicio de sesión: {str(e)}'}), 500
 
 @app.route('/users', methods=['GET'])
 def get_users():
