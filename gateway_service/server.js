8472c7db4a53db36bfe4842d761473ec5da2e4bf
Jhon Freddy Popo Moreno <jhonfreddypopomoreno@gmail.com>
2023-12-10 00:15:51 -0500

    Organizado inicio de session, token cabecera, logout

--- gateway_service/server.js
+++ gateway_service/server.js
@@ -7,6 +7,19 @@ const port = 5002;
 
 const userServiceUrl = "http://users_service:5000";
 
+// Función para configurar el token en las solicitudes Axios
+const setAxiosHeaders = () => {
+  if (globalToken) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${globalToken}`;
+  } else {
+    delete axios.defaults.headers.common['Authorization'];
+  }
+};
+
+// Variable global para almacenar el token
+let globalToken = null;
+let globalDataUsuario = null;
+
 const dns = require('dns');
 
 dns.lookup('users_service', (err, address, family) => {
@@ -30,6 +43,10 @@ app.post('/login', async (req, res) => {
   console.log("Recibida solicitud en /login");
   try {
     const response = await axios.post(`${userServiceUrl}/login`, req.body);
+    globalToken = response.data.token; // Almacena el token globalmente
+    globalDataUsuario = response.data.user; // Almacena el usuario globalmente
+    setAxiosHeaders(); // Configura el token en las solicitudes Axios
+    console.log('token almacenado', globalToken);
     res.status(response.status).json(response.data);
   } catch (error) {
     console.error("Error en el inicio de sesión", error.message);
@@ -40,7 +57,15 @@ app.post('/login', async (req, res) => {
 app.get('/users', async (req, res) => {
   console.log("Recibida solicitud en /users");
   try {
-    const response = await axios.get(`${userServiceUrl}/users`, req.body);
+    // Verifica si el usuario está autenticado
+    if (!globalToken) {
+      return res.status(401).json({ message: 'No autorizado. Inicie sesión para acceder.' });
+    }
+
+    // Configura el token en las solicitudes Axios
+    setAxiosHeaders();
+
+    const response = await axios.get(`${userServiceUrl}/users`);
     res.status(response.status).json(response.data);
   } catch (error) {
     console.error("Error en buscar usuarios", error.message);
@@ -51,6 +76,14 @@ app.get('/users', async (req, res) => {
 app.get('/hello', (req, res) => {
   console.log("Recibida solicitud en /hello");
   res.status(200).json({ message: 'Hola' });
+});
+
+// Ruta de logout
+app.post('/logout', (req, res) => {
+  console.log("Recibida solicitud en /logout");
+  globalToken = null; // Invalida el token almacenado
+  setAxiosHeaders(); // Configura el token en las solicitudes Axios (lo eliminará)
+  res.status(200).json({ message: 'Logout exitoso' });
 });
 
 app.listen(port, () => {
--- users_service/users_service.py
+++ users_service/users_service.py
@@ -10,6 +10,23 @@ app.config['SECRET_KEY'] = 'mi_clave_secreta_jwt'
 # Simulación de una base de datos (deberías usar una base de datos real)
 users_db = []
 
+# Variable global para almacenar el token
+global_token = None
+
+# Simulación de una lista de tokens revocados
+revoked_tokens = set()
+
+def verify_token(token):
+    if token in revoked_tokens:
+        return False
+    try:
+        decoded = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        return decoded['username']
+    except jwt.ExpiredSignatureError:
+        return 'Token expirado'
+    except jwt.InvalidTokenError:
+        return 'Token inválido'
+
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
@@ -57,7 +74,18 @@ def login():
     try:
         if user is not None and check_password_hash(user['password'], password):
             token = jwt.encode({'username': username}, app.config['SECRET_KEY'], algorithm='HS256')
-            return jsonify({'token': token}), 200
+            
+            global_token = token
+
+            datos_usuario = {
+                'username': user['username'],
+                'email': user['email'],
+                'nombre': user['nombre'],
+                'apellido': user['apellido'],
+                'telefono': user['telefono']
+            }       
+
+            return jsonify({'token': global_token ,'user': datos_usuario}), 200
             
         else:
             print(f"Error en el inicio de sesión para el usuario {username}")
@@ -69,7 +97,31 @@ def login():
 
 @app.route('/users', methods=['GET'])
 def get_users():
+    global global_token
+    token = request.headers.get('Authorization')
+
+    if not token:
+        return jsonify({'message': 'Token no proporcionado'}), 401
+
+    username = verify_token(token)
+    
+    if not username:
+        return jsonify({'message': 'Token inválido o expirado'}), 401
+
+    # Configura el token en las solicitudes a otras rutas
+    global_token = token
+
     return jsonify({'users': users_db}), 200
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    global global_token
+    if global_token:
+        revoked_tokens.add(global_token)  # Agrega el token a la lista de revocados
+        global_token = None
+        return jsonify({'message': 'Logout exitoso'}), 200
+    else:
+        return jsonify({'message': 'No hay sesión activa'}), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
